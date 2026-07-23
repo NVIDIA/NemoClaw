f1c79e98790ddc90abfa6f6c424f63b5da4f40c1
Remy the Refactoror <jmyaunch@gmail.com>
2026-07-23 13:33:28 +0000

    feat(readiness): project host observations into readiness report
    Signed-off-by: Remy the Refactoror <jmyaunch@gmail.com>

--- src/lib/readiness/index.ts
+++ src/lib/readiness/index.ts
@@ -5,6 +5,17 @@ export type { SchemaCompatibility } from "./compatibility.js";
 export { checkSystemReadinessSchemaVersion } from "./compatibility.js";
 export { getSystemReadinessReferenceErrors } from "./references.js";
 export {
+  collectHostObservations,
+  createHostReadinessReport,
+  projectHostReadiness,
+} from "./host.js";
+export type {
+  CollectHostObservationsOptions,
+  CreateHostReadinessReportOptions,
+  HostObservationSnapshot,
+  HostObservations,
+} from "./host.js";
+export {
   SUPPORTED_SYSTEM_READINESS_SCHEMA_MAJOR,
   SYSTEM_READINESS_SCHEMA_VERSION,
 } from "./types.js";
